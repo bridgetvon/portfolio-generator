437d941e53dd7291ce835115a854f97f2ca8dc87
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-02 07:02:15 -0600

    updated formatting

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    for (let i =0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-}
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-console.log('================');
+  console.log('================');
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
