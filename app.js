41ce7069193173c9884e0c6fa1040de1d2524a9b
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-02 22:09:08 -0600

    installed inquirer left off at 9.3.5 after the console log to test correct installation

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [userName , github] = profileDataArgs;
+const inquirer = require('inquirer');
 
-//User input 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(userName, github);
 
 //create an HTML template 
 
-fs.writeFile('./index.html', generatePage(userName, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! check out the index.html');
-});
+// fs.writeFile('./index.html', generatePage(userName, github), err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! check out the index.html');
+// });
+
