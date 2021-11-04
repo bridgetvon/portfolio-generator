75a6e66ddcf0bb5ff0dff732f57bef8691e5f081
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-03 18:35:22 -0600

    started ctyling with dist folder

--- app.js
+++ app.js
@@ -139,13 +139,22 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw err;
-
+        fs.writeFile('./dist/index.html' , pageHTML, err => {
+        if (err) 
+        console.log(err);
+        return;
+        },
         console.log('Page created! Check out index.html in this directory to see it!');
-
+        
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('style sheet copied successfully!');
+        });
     });
-});
+
 
 // //create mock data for testing 
 // const mockData = {
